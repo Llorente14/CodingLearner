26c2490d6b51624364753f59f5ab28abaaf5afeb
Llorente14 <axel.crisdy1980@gmail.com>
2023-08-15 18:46:18 +0700

    Chapter1

---
+++ jawa.js
@@ -0,0 +1,9 @@
+console.log("Mantap");
+
+let number1 = 10;
+let number3 = 51;
+console.log(number3 % number1);
+
+let namadepan = "ilham";
+let namablkng = "Suitio";
+console.log(namadepan + "  " + namablkng);
